c3f6188ecd713fc67c7924605372b6559935b277
PavelOrnovskis <pavel.ornovskis@gmail.com>
2024-12-20 02:09:03 +0200

    Added tests for new functions

--- test/lat.test.ts
+++ test/lat.test.ts
@@ -1,11 +1,11 @@
 import { lat } from '../src/lat'
 
 test('Check Latvia prefix and number', () => {
-    const actualResult = lat()
-    const expectedResult = 'LVA-3710'
+  const actualResult = lat()
+  const expectedResult = 'LVA-3710'
 
-    expect(actualResult).toBe(expectedResult)
-    expect(actualResult.startsWith('LVA-')).toBe(true)
-    expect(actualResult.length).toBe(8)
-    expect(/LVA-\d{4}/.test(actualResult)).toBe(true)
+  expect(actualResult).toBe(expectedResult)
+  expect(actualResult.startsWith('LVA-')).toBe(true)
+  expect(actualResult.length).toBe(8)
+  expect(/LVA-\d{4}/.test(actualResult)).toBe(true)
 })
--- test/lit.test.ts
+++ test/lit.test.ts
@@ -1,11 +1,11 @@
 import { lit } from '../src/lit'
 
 test('Check Lithuania prefix and number', () => {
-    const actualResult = lit()
-    const expectedResult = 'LTU-3700'
+  const actualResult = lit()
+  const expectedResult = 'LTU-3700'
 
-    expect(actualResult).toBe(expectedResult)
-    expect(actualResult.startsWith('LTU-')).toBe(true)
-    expect(actualResult.length).toBe(8)
-    expect(/LTU-\d{4}/.test(actualResult)).toBe(true)
+  expect(actualResult).toBe(expectedResult)
+  expect(actualResult.startsWith('LTU-')).toBe(true)
+  expect(actualResult.length).toBe(8)
+  expect(/LTU-\d{4}/.test(actualResult)).toBe(true)
 })
