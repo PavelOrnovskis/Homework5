2c5e957a8ad2379bc9175273960ba6f77e3393e3
PavelOrnovskis <pavel.ornovskis@gmail.com>
2024-12-22 15:37:06 +0200

    Fixed test accordingly to the tests with a random 4 digit

--- test/est.test.ts
+++ test/est.test.ts
@@ -2,9 +2,7 @@ import { est } from '../src/est'
 
 test('Check Estonia prefix and number', () => {
   const actualResult = est()
-  const expectedResult = 'EST-3720'
 
-  expect(actualResult).toBe(expectedResult)
   expect(actualResult.startsWith('EST-')).toBe(true)
   expect(actualResult.length).toBe(8)
   expect(/EST-\d{4}/.test(actualResult)).toBe(true)
--- test/lat.test.ts
+++ test/lat.test.ts
@@ -2,9 +2,7 @@ import { lat } from '../src/lat'
 
 test('Check Latvia prefix and number', () => {
   const actualResult = lat()
-  const expectedResult = 'LVA-3710'
 
-  expect(actualResult).toBe(expectedResult)
   expect(actualResult.startsWith('LVA-')).toBe(true)
   expect(actualResult.length).toBe(8)
   expect(/LVA-\d{4}/.test(actualResult)).toBe(true)
